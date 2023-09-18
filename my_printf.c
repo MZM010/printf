fae6c0db189de692c7fb04f1bdabacfe910d04c7
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-18 12:26:10 -0700

    printf

--- my_printf.c
+++ my_printf.c
@@ -13,6 +13,10 @@ int _printf(const char *format, ...)
 	int counter = 0;
 
 	va_start(list, format);
+	if (!format || (format[0] == '%' && !format[1]))
+        	return (-1);
+    	if (format[0] == '%' && format[1] == ' ' && !format[2])
+        	return (-1);
 	while (*format)
 	{
 		if (*format == '%')
