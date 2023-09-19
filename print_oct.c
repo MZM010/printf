6f60bf353c35f2228888d72a93e54f74c9011083
Mohamed zouine <mohamedzouine827@gmail.com>
2023-09-19 12:33:31 -0700

    printf

--- print_oct.c
+++ print_oct.c
@@ -11,22 +11,24 @@ int print_oct(va_list list)
 	unsigned int n = va_arg(list, unsigned int);
 	int len = 0, i = 0;
 	int octal[150];
-
+	int octall = 0;
 	do
 	{
 		octal[i++] = n % 8;
 		n /= 8;
 	}while (n != 0);
+
 	if (i == 0)
 	{
-		my_putchar('0');
+		my_putchar(48);
 		len++;
 	}
 	i = i - 1;
 
 	for (i = i; i >= 0; i--)
 	{
-		my_putchar(octal[i] + '0');
+		octall = octal[i];
+		my_putchar(octall + 48);
 		len++;
 	}
 
